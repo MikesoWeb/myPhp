8d3d0952da237d3329bdfada87dd595d776f443a
MikesoWeb <klionpsyx@gmail.com>
2019-03-24 16:08:48 +0300

    added enter.php

---
+++ enter.php
@@ -0,0 +1,9 @@
+<p><a href="index.php">Главная</a> | <a href="contact.php">Контакты</a> | <a href="admin.php">Админка</a></p>
+<hr />
+Это страница авторизации.
+<br />
+<form method="post">
+ Username: <input type="text" name="user" /><br />
+ Password: <input type="password" name="pass" /><br />
+ <input type="submit" name="submit" value="Войти" />
+</form>
