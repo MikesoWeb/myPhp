720a9c570026f2e35c00fabcf7888d42ff7ae1fb
MikesoWeb <klionpsyx@gmail.com>
2019-03-24 17:18:49 +0300

    Добавлен файл news

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 	<body><p>
 
-<p><a href="index.php">Главная</a> | <a href="contact.php">Контакты</a> | <a href="admin.php">Админка</a></p>
+<p><a href="index.php">Главная</a> | <a href="contact.php">Контакты</a> | <a href="news.php">Новости</a> | <a href="admin.php">Админка</a></p>
 <hr />
 Это главная страница
 
