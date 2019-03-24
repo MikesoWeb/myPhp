1c6c7abef353b347a8ab06ac0dc0830f4541f2fd
MikesoWeb <klionpsyx@gmail.com>
2019-03-24 17:12:09 +0300

    смена локал дистр

--- index.php
+++ index.php
@@ -3,41 +3,17 @@
 <html>
 <head>
 	<title> First page on php </title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 
 	<body><p>
 
+<p><a href="index.php">Главная</a> | <a href="contact.php">Контакты</a> | <a href="admin.php">Админка</a></p>
+<hr />
+Это главная страница
 
 
 
-<?php
-
-$branch = "Add branch";
-$users ["Trestol",
-		"Pupkin",
-		"Wertic",
-		"Cosmos"];
-
-		foreach ($users as $user) {
-
-			echo $user;
-			echo $branch;
-		
-		}
-
-
-$str = 'Пароль';
-echo '<br>';
-echo md5($str);
-echo '<br>';
-
-if (md5($str) === '5ebe553e01799a927b1d045924bbd4fd')
-	{echo "Проверка пройдена!<br>";
-}
-
-
-?>
 
 </p>
 </body>
