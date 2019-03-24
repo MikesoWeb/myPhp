481cfda3a80246f82254732c74254603962e952c
MikesoWeb <klionpsyx@gmail.com>
2019-03-24 15:45:55 +0300

    added menu

--- index.php
+++ index.php
@@ -8,11 +8,14 @@
 
 	<body><p>
 
-
+<p><a href="index.php">Главная</a> | <a href="contact.php">Контакты</a> | <a href="admin.php">Админка</a></p>
+<hr />
+Это главная страница
 
 
 <form action="action.php" method="post">
- <p>Ваше имя: <input type="text" name="name" /></p>
+ <p>Ваше имя:   <input type="text" name="name" /></p>
+ <p>Ваш пароль: <input type="text" name="pass" /></p>
  
  <p><input type="submit" /></p>
 </form>
